c50edca6418a9b07260a81cb7c830535ee9dbda4
Morde Yoiris <morde1515@gmail.com>
2021-01-02 14:40:24 +0200

    last commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,12 +5,12 @@ export default function Body() {
     <div id='Me' className='text-center bodyDiv'>
       <div className='spacing'>
         <img className='myImg' src='images/profilepic.png' alt='profilepic' />
-        <h2 className='spacing'>
+        <h2 className='spacing myH2'>
           Welcome to my website, My name is Morde Yoiris and I'm currently
           working as a freelancer web developer , building web apps and sites
           for different businesses.
         </h2>
-        <p>
+        <p className='myP'>
           I have developed a diverse set of skills during my time as a developer
           that I would like to share with you, keep scrolling to get more info.
         </p>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,20 @@ export default function Footer() {
         I hope you enjoyed my work, if you did then get In touch with me!
       </h2>
       <a className='spacingIcons' href={"mailTo: morde1515@gmail.com"}>
-        <img src='/images/gmail.png' alt='gmailIcon' />
+        <img className='iconJump' src='/images/gmail.png' alt='gmailIcon' />
       </a>
-      <a className='spacingIcons' href='https://www.linkedin.com/'>
-        <img src='/images/linkedin.png' alt='linkedinIcon' />
+      <a
+        className='spacingIcons'
+        href='https://www.linkedin.com/in/morde-yoiris-287006138/'
+      >
+        <img
+          className='iconJump'
+          src='/images/linkedin.png'
+          alt='linkedinIcon'
+        />
       </a>
       <a className='spacingIcons' href='https://www.facebook.com/morde.yoiris/'>
-        <img src='/images/facebook.png' alt='' />
+        <img className='iconJump' src='/images/facebook.png' alt='' />
       </a>
       <p className='spacing'>©{new Date().getFullYear()} Morde Yoiris.</p>
     </div>
